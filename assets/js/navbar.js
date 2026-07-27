7692184bf205c3fdd7ab309832bb9f6201c4facd
Neto Sbarai <netosbarai@hotmail.com>
2026-07-27 13:59:33 -0300

    feat: smart navbar and scroll indicator
    Adiciona navbar dinâmica com efeito de blur durante a rolagem e indicador visual animado para incentivar a navegação pela página.

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,1 +1,15 @@
-console.log("🚀 Neto Sbarai iniciado!");
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 40){
+
+        header.classList.add("scrolled");
+
+    }else{
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
